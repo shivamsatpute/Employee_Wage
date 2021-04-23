ac7b5579e0db4cd5095c23bb894b76407204bdb8
shivam satpute <shivam897satpute@gmail.com>
2021-04-23 22:44:25 +0530

    added: wageFor_month

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -2,15 +2,25 @@
 
 isPartTime=1
 isFullTime=2
+totalSalary=0
 empRatePerHr=20
-randomCheck=$((RANDOM%3))
+numWorkingDays=20
 
-case $empCheck in
-	$isFullTime) empHrs=8 ;;
-	$isPartTime) empHrs=4 ;;
-	*) empHrs=0 ;;
-esac
+for (( i=1 ; i<=$numWorkingDays ; i++))
+do
+	empCheck=$((RANDOM%3))
+		case $empCheck in
+			$isFullTime) empHrs=8 ;;
+			$isPartTime) empHrs=4 ;;
+			*) empHrs=0 ;;
+		esac
 
-salary=$(($empHrs*$empRatePerHr))
+		salary=$(($empHrs*$empRatePerHr))
+		totalSalary=$(($totalSalary+$salary))
 
-echo "Part Time Employee Wage :" $salary
+done
+
+
+echo "Wages for a Month:" $salary
+echo "Total Salary:" $totalSalary
+
