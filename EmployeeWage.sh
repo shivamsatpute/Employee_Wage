1c0387c9a76d0a2410b033f0d96bb4c71bd8b542
shivam satpute <shivam897satpute@gmail.com>
2021-04-23 22:38:20 +0530

    added:Daily EmployeeWageUsingSwitch

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
-IS_PRESENT_FULL_TIME=1
-IS_PRESENT_PART_TIME=2
-EMP_RATE_PER_HR=20
-empCheck=$(( $RANDOM % 3))
-if [ $empCheck -eq $IS_PRESENT ]
-then
-echo " Employee is present"
-EMP_RATE_HR=20
-empHrs=8
- SALARY=$(( $empHrs * $EMP_RATE_HR ))
-echo "salary: $salary"
-else
-echo "Employee is absent"
-echo "salary=0"
-fi
+
+isPartTime=1
+isFullTime=2
+empRatePerHr=20
+randomCheck=$((RANDOM%3))
+
+case $empCheck in
+	$isFullTime) empHrs=8 ;;
+	$isPartTime) empHrs=4 ;;
+	*) empHrs=0 ;;
+esac
+
+salary=$(($empHrs*$empRatePerHr))
+
+echo "Part Time Employee Wage :" $salary
