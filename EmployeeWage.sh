385b1df417ff4f389f5c9352a9131680cbb83c99
shivam satpute <shivam897satpute@gmail.com>
2021-04-24 15:18:36 +0530

    added: getWorkHrsFun

--- EmployeeWage.sh
+++ EmployeeWage.sh
@@ -1,14 +1,31 @@
 #! /bin/bash
 
-isPresent=1
-randomCheck=$((RANDOM%2))
+isPartTime=1
+isFullTime=2
+maxHrsInMonth=10
+empRatePerHr=20
+numWorkingDays=20
 
-if [ $isPresent -eq $randomCheck ]
-then
-	empRatePerHr=20
-	empHrs=8
-	salary=$(($empHrs*$empRatePerHr))
-else
-	salary=0
-fi
-echo "Employee Daily Wage is :" $salary
+totalEmpHrs=100
+totalWorkingDays=20
+
+function getWorkingHours() {
+	case $1 in
+		$isFullTime) workHrs=8 ;;
+		$isPartTime) workHrs=4 ;;
+		*) workHrs=0 ;;
+	esac
+	echo "Working Hours :" $workHrs
+}
+
+while [[ $totalWorkHrs -lt $maxHrsInMonth && $totalWorkingDays -lt $numWokingDays ]]
+do
+	((totalWorkingDays++))
+	workHrs"$( getWorkingHours $((RANDOM)) )"
+	totalWorkHrs=$(($totalWorkHrs*$workHrs))
+
+done
+
+totalSalary=$(($totalWorkHrs*$workHrs))
+
+echo "Total Salary:" $totalSalary
